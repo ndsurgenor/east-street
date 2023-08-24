8cabfcaee35331b62b8e6821e72405ba9321167d
Nathan Surgenor <ndsurgenor@gmail.com>
2023-08-24 09:38:07 +0000

    Add menu and location .html along with relevant urls/views

--- booking_sys/urls.py
+++ booking_sys/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.menu, name='menu'),
+    path('location/', views.location, name='location'),
     path('bookings/', views.UserBookings.as_view(), name='bookings')
 ]
--- booking_sys/views.py
+++ booking_sys/views.py
@@ -8,6 +8,14 @@ def index(request):
     return render(request, 'index.html')
 
 
+def menu(request):
+    return render(request, 'menu.html')
+
+
+def location(request):
+    return render(request, 'location.html')
+
+
 class UserBookings(generic.ListView):
     model = Booking
     template_name = 'bookings.html'

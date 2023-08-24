34a66242708c4e9137e80d5679d521e9d0323db5
Nathan Surgenor <ndsurgenor@gmail.com>
2023-08-24 11:13:44 +0000

    Link style.css to html files

--- eaststreet/settings.py
+++ eaststreet/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')

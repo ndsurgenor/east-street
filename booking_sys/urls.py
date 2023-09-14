a0384366130efd579ce878c9e50d2e6a0e57b523
Nathan Surgenor <ndsurgenor@gmail.com>
2023-09-14 22:35:07 +0000

    Add Update view to allow user changes of bookings

--- booking_sys/urls.py
+++ booking_sys/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('location/', views.location, name='location'),
     path('bookingsite/', views.BookFormView.as_view(), name='bookingsite'),
     path('userbookings/', views.BookListView.as_view(), name='userbookings'),
+    path('<pk>/update/', views.BookUpdateView.as_view(), name='update'),
     path('<pk>/delete/', views.BookDeleteView.as_view(), name='delete'),
-    path('<slug:slug>/', views.BookDetail.as_view(), name='detail'),
 ]
--- booking_sys/views.py
+++ booking_sys/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic, View
+from django.contrib import messages
 from .models import Booking
 from .forms import BookingForm
 
@@ -44,23 +45,28 @@ class BookListView(generic.ListView):
     template_name = 'userbookings.html'
 
 
+# Allows a user to update a booking
+class BookUpdateView(generic.UpdateView):
+    model = Booking
+    form_class = BookingForm
+    template_name = 'bookingform.html'
+    success_url = '/userbookings'
+
+    def form_valid(self, form):
+        # form.instance.contact_id = self.request.user.id
+        messages.success(self.request, 'The booking was successfully updated')
+        return super(BookUpdateView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        for field, errors in form.errors.items():
+            print(f"Field: {field}")
+            for error in errors:
+                print(f"Error: {error}")
+        return HttpResponse(f'Form invalid')
+
+
 # Allows a user to delete a booking
 class BookDeleteView(generic.DeleteView):
     model = Booking
     template_name = 'bookingdelete.html'
     success_url = '/userbookings'
-
-
-class BookDetail(View):
-
-    def get(self, request, reference, *args, **kwargs):
-        queryset = Booking.objects
-        booking = get_object_or_404(queryset, slug=reference)
-
-        return render(
-            request,
-            'bookingdetail.html',
-            {
-                'booking': booking,
-            },
-        )

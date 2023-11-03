cf52469a0ab033c9c22ceda9c4a95a7bd8045121
Nathan Surgenor <ndsurgenor@gmail.com>
2023-11-03 09:15:55 +0000

    Update admin.py model with readonly fields

--- booking_sys/admin.py
+++ booking_sys/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Booking
 
+
+class BookingAdmin(admin.ModelAdmin):
+    readonly_fields = ['contact', 'date', 'time', 'group']
+
+
 admin.site.register(Booking)

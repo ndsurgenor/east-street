746e86eb1525841aed1455d14ed34b00159e1396
Nathan Surgenor <ndsurgenor@gmail.com>
2023-11-05 17:08:58 +0000

    Add test cases for updating/deletion by user who is not the owner of said booking

--- booking_sys/tests.py
+++ booking_sys/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_update_forbidden(self):
+        """
+        Tests that booking cannot be updated by user who is not the owner
+        """
+        user_model = get_user_model()
+        bad_username = 'badtest'
+        bad_password = 'test#666'
+        user = user_model.objects.create_user(
+            username=bad_username,
+            password=bad_password,
+            is_superuser=False,
+        )
+        signed_in = self.client.login(
+            username=bad_username,
+            password=bad_password,
+        )
+
+        self.assertTrue(signed_in)
+        response = self.client.get('/10/update')
+        self.assertEqual(response.status_code, 301)
+    
+    def test_delete_forbidden(self):
+        """
+        Tests that booking cannot be deleted by user who is not the owner
+        """
+        user_model = get_user_model()
+        bad_username = 'badtest'
+        bad_password = 'test#666'
+        user = user_model.objects.create_user(
+            username=bad_username,
+            password=bad_password,
+            is_superuser=False,
+        )
+        signed_in = self.client.login(
+            username=bad_username,
+            password=bad_password,
+        )
+
+        self.assertTrue(signed_in)
+        response = self.client.get('/10/delete')
+        self.assertEqual(response.status_code, 301)
--- eaststreet/settings.py
+++ eaststreet/settings.py
@@ -97,18 +97,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'eaststreet.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+# https://docs.djangoproject.com/en/3.2/ref/settings/databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
